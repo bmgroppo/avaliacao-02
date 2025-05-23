a4f37469d7bef814ad4d1f77725848378fab696d
Bruno Groppo <bmgroppo@gmail.com>
2025-05-23 10:37:46 -0300

    Exercício3

--- exercicio3.js
+++ exercicio3.js
@@ -1,1 +1,16 @@
+function tabuadaPersonalizada() {
+    let numeroBase = Number(prompt("Digite o número base da tabuada:"));
+    let numeroFinal = Number(prompt("Digite até onde deseja calcular a tabuada:"));
 
+    if (isNaN(numeroBase) || isNaN(numeroFinal) || numeroFinal < 1) {
+        document.writeln("Por favor, insira números válidos!");
+        return;
+    }
+
+    document.writeln(`<h3>Tabuada do número ${numeroBase} até ${numeroFinal}</h3>`);
+
+    for (let i = 1; i <= numeroFinal; i++) {
+        let resultadoMultiplicacao = numeroBase * i;
+        document.writeln(`${numeroBase} x ${i} = ${resultadoMultiplicacao} <br>`);
+    }
+}
