9fc49f36a4bec47f4601ce1fe3b5ec9544c700df
Bruno Groppo <bmgroppo@gmail.com>
2025-05-23 10:59:51 -0300

    exercicio5

--- exercicio5.js
+++ exercicio5.js
@@ -1,1 +1,23 @@
+function validaSenha() {
+    let senhaCorreta = "senai123";
+    let tentativas = 3;
+    let mensagem;
+
+    while (tentativas > 0) {
+        let senhaUsuario = prompt(`Digite a senha (${tentativas} tentativa(s) restante(s)):`);
+
+        if (senhaUsuario === senhaCorreta) {
+            mensagem = ("Acesso permitido!");
+            break
+        } 
+        tentativas--;
+
+        if (tentativas > 0) {
+            mensagem = ("Senha incorreta! Tente novamente.");
+        } else {
+            mensagem = ("Acesso negado! Você excedeu o número de tentativas.");                
+            }
+        }
+        document.writeln(mensagem);
+    }
 
