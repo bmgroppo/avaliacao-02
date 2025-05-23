1d989f1b9fe225978cb122c9cf103e514e6acc28
Bruno Groppo <bmgroppo@gmail.com>
2025-05-23 10:41:14 -0300

    exercicio4

--- exercicio4.js
+++ exercicio4.js
@@ -1,1 +1,18 @@
+function somaParesWhile() {
+    let limite = Number(prompt("Digite um número limite para somar todos os pares no intervalo:"));
 
+    if (isNaN(limite) || limite < 1) {
+        document.writeln("Por favor, insira um número válido maior que zero!");
+        return;
+    }
+
+    let soma = 0;
+    let numero = 2;
+
+    while (numero <= limite) {
+        soma += num;
+        numero += 2;
+    }
+
+    document.writeln(`A soma de todos os números pares até ${limite} é: ${soma}`);
+}
