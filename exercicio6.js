83fd323b6ffb774e1301dba273b9fa4910c64027
Bruno Groppo <bmgroppo@gmail.com>
2025-05-23 11:04:02 -0300

    exercicio6

--- exercicio6.js
+++ exercicio6.js
@@ -1,1 +1,15 @@
+function calcularFatorial() {
+    let fatorial = 1;
+    let numeroInformado = Number(prompt("Digite um número inteiro positivo para calcular o fatorial:"));
 
+    if (isNaN(numeroInformado) || numeroInformado < 0) {
+        document.writeln("Por favor, insira um número inteiro positivo válido!");
+        return;
+    }
+ 
+    for (let i = 1; i <= numeroInformado; i++) {
+        fatorial *= i;
+    }
+
+    document.writeln(`O fatorial de ${numeroInformado} é: ${fatorial}`);
+}
